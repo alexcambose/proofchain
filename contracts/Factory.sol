74de31980055685c1f63eb3ef79a1a0a40608588
alexcambose <alexcambose@yahoo.com>
2021-02-24 14:16:06 +0200

    Factory contract

---
+++ contracts/Factory.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.7.0 <0.9.0;
+
+import "./Company.sol";
+import "./RawMaterial.sol";
+
+contract Factory {
+    address companyContractAddress;
+    address rawMaterialContractAddress;
+
+    constructor() {
+        Company company = new Company();
+        companyContractAddress = address(company);
+        RawMaterial rawMaterial = new RawMaterial();
+        rawMaterialContractAddress = address(rawMaterial);
+    }
+}
