c28103b690c7bb6927fe32fc417fd22384970b95
alexcambose <alexcambose@yahoo.com>
2021-03-10 22:30:18 +0200

    feat(truffle): adde testnet config

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   // Uncommenting the defaults below
   // provides for an easier quick-start with Ganache.
@@ -15,6 +17,15 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(
+          "b1d7f93e7a535ec11a5f99a358cd30d76de52a191e7872b304a5d940b77e743d",
+          "https://rinkeby.infura.io/v3/27c99ce149ba413080a05eff7597f5cc"
+        );
+      },
+      network_id: 4,
     },
   },
   compilers: {
