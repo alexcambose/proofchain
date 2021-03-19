6e69629ecb3296f8b6f87ac5ea62f7f0e38cdeda
alexcambose <alexcambose@yahoo.com>
2021-03-19 13:30:43 +0200

    feat(truffle): export contract to env

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,33 @@
-const Factory = artifacts.require('Factory');
-
-module.exports = function (deployer) {
-  deployer.deploy(Factory);
+const Factory = artifacts.require("Factory");
+const { parse, stringify } = require("envfile");
+const fs = require("fs");
+const CFonts = require("cfonts");
+module.exports = async (deployer) => {
+  await deployer.deploy(Factory);
+  CFonts.say("Proofchain", {
+    font: "tiny",
+    align: "center",
+    background: "blackBright",
+    space: true,
+    env: "node",
+  });
+  console.log("=========================");
+  console.log("Writing to config file...");
+  const dotenvContent = parse(fs.readFileSync("../.env"));
+  if (process.env.NETWORK_TYPE === "production") {
+    dotenvContent["PRODUCTION_FACTORY_CONTRACT_ADDRESS"] = Factory.address;
+  } else {
+    dotenvContent["DEVELOPMENT_FACTORY_CONTRACT_ADDRESS"] = Factory.address;
+  }
+  fs.writeFileSync("../.env", stringify(dotenvContent));
+  console.log("Done");
+  console.log("Contract address: ");
+  CFonts.say(Factory.address, {
+    font: "console",
+    color: "green",
+    align: "center",
+    space: true,
+    env: "node",
+  });
+  console.log("=========================");
 };
