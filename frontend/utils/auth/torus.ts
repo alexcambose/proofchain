7a6674f994629f6757b0d0867d402af34988d358
alexcambose <alexcambose@yahoo.com>
2021-06-14 19:56:18 +0300

    fix(frontend): torus path

--- frontend/utils/auth/torus.ts
+++ frontend/utils/auth/torus.ts
@@ -1,13 +1,17 @@
 import DirectWebSdk from '@toruslabs/torus-direct-web-sdk';
 import { SocialLoginTypeEnum } from 'enums';
 import config from 'config';
-const torus = new DirectWebSdk({
-  baseUrl: 'http://localhost:3000',
-  enableLogging: true,
+import { isClient } from '@utils/next';
+let torus = null;
+if (isClient()) {
+  torus = new DirectWebSdk({
+    baseUrl: window.location.origin,
+    enableLogging: true,
 
-  proxyContractAddress: '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183', // details for test net
-  network: 'testnet', // details for test net
-});
+    proxyContractAddress: '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183', // details for test net
+    network: 'testnet', // details for test net
+  });
+}
 
 export const init = async () => {
   await torus.init();
